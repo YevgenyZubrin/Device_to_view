e6084ec286cc1ec0483283c2ec561e0039fdf30a
YevgenyZubrin <yevgeniy.zubrin@gmail.com>
2021-02-15 09:01:40 +0600

    fixed search field active-bihevior

--- js/search-focus.js
+++ js/search-focus.js
@@ -2,30 +2,40 @@ var searchForm = document.querySelector('.search-form');
 var searchButton = document.querySelector('.search-form__button');
 var searchInput = document.querySelector('.search-form__input');
 
-searchInput.addEventListener('click', function(evt) {
+searchInput.addEventListener('focus', function(evt) {
     evt.preventDefault();
-        if (!searchForm.classList.contains('page-header__form_search-focus') && !searchButton.classList.contains('search-form__button_focus')) {
+        if (!searchForm.classList.contains('page-header__form_search-focus') && searchButton.classList.contains('visually-hidden')) {
             searchForm.classList.add('page-header__form_search-focus');
-            searchButton.classList.add('search-form__button_focus');
+            searchButton.classList.remove('visually-hidden');
         }
 });
 
-
-window.addEventListener('keydown', function(evt) {
-    if (evt.keyCode === 27) {
-        if (searchForm.classList.contains('page-header__form_search-focus') && searchButton.classList.contains('search-form__button_focus')) {
+searchInput.addEventListener('blur', function(evt) {
     evt.preventDefault();
-
+        if (searchForm.classList.contains('page-header__form_search-focus')) { 
             searchForm.classList.remove('page-header__form_search-focus');
-            searchButton.classList.remove('search-form__button_focus');
         }
-    }
+
+        if (!searchButton.classList.contains('visually-hidden')) {
+            searchButton.classList.add('visually-hidden');
+        }
 });
+
+// window.addEventListener('keydown', function(evt) {
+//     if (evt.keyCode === 27) {
+//         if (searchForm.classList.contains('page-header__form_search-focus') && !searchButton.classList.contains('visually-hidden')) {
+//     evt.preventDefault();
+
+//             searchForm.classList.remove('page-header__form_search-focus');
+//             searchButton.classList.add('visually-hidden');
+//         }
+//     }
+// });
 
 searchForm.addEventListener('submit', function(evt) {
     
-    if (searchForm.classList.contains('page-header__form_search-focus') && searchButton.classList.contains('search-form__button_focus')) {
+    if (searchForm.classList.contains('page-header__form_search-focus') && !searchButton.classList.contains('visually-hidden')) {
                 searchForm.classList.remove('page-header__form_search-focus');
-                searchButton.classList.remove('search-form__button_focus');
+                searchButton.classList.add('visually-hidden');
             }
 })
